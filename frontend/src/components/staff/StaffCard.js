db2b898c3aaeda13a86248d74fa085bc8e1eafc0
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2024-03-28 12:46:44 +0500

    added staff del functionality

--- frontend/src/components/staff/StaffCard.js
+++ frontend/src/components/staff/StaffCard.js
@@ -1,7 +1,33 @@
 import React from "react";
 import "./StaffCard.scss"; // Import styles for the staff card
 import {  FaTrashAlt } from "react-icons/fa";
-const StaffCard = ({ user }) => {
+const StaffCard = ({ restaurantId , user }) => {
+  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+  console.log(user);
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(
+        `${BACKEND_URL}/api/restaurants/delete_staff?staffId=${user._id}&restaurantId=${restaurantId}`, 
+        { 
+          method: 'DELETE',
+          credentials: 'include', // Ensure cookies are included with the request
+          headers: {
+            'Content-Type': 'application/json',
+            // You don't manually set the token here since it's automatically included with cookies
+          },
+        }
+      );
+  
+      if (response.ok) {
+        console.log("Staff member deleted successfully");
+        // Optionally, trigger a UI update or redirect
+      } else {
+        console.error("Failed to delete staff member");
+      }
+    } catch (error) {
+      console.error("Error deleting staff member:", error);
+    }
+  };
   return (
     <div className="staff-card" style={{ width: "70%" }}>
       <img src={user.photo} alt={user.name} className="staff-photo" />
@@ -16,6 +42,7 @@ const StaffCard = ({ user }) => {
             <FaTrashAlt
               size={20}
               color={"red"}
+              onClick={handleDelete}
             />
           </span>
       </div>
