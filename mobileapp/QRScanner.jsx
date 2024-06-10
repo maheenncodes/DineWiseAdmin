611336eea7624a73d5bb2e9d50f553bf6f3d5682
Fatima Siddiqui <fatimasidd440@gmail.com>
2024-06-10 15:28:01 +0500

    smoll bugs

--- mobileapp/QRScanner.jsx
+++ mobileapp/QRScanner.jsx
@@ -3,17 +3,38 @@ import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { addToTable } from './api-scan'; // Import the API function
 import { AuthContext } from './authcontext'; // Import AuthContext
+import { useNavigation } from '@react-navigation/native';
+import QRScanContext from './QRScanContext';
 
 const QRScanner = () => {
     const { user } = useContext(AuthContext); // Access user authentication state from context
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
+    const navigation = useNavigation();
+    const { handleScan } = useContext(QRScanContext);
 
     useEffect(() => {
         (async () => {
             const { status } = await BarCodeScanner.requestPermissionsAsync();
             setHasPermission(status === 'granted');
         })();
+
+        const ws = new WebSocket('ws://192.168.1.13:5000');
+
+        ws.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            if (message.userId === user._id) {
+                // User themselves have scanned
+                Alert.alert('Success', 'You have been successfully added to the table');
+            } else {
+                // Someone else has been added
+                Alert.alert('Success', 'A new user has been added');
+            }
+        };
+
+        return () => {
+            ws.close();
+        };
     }, []);
 
     const handleBarCodeScanned = async ({ data }) => {
@@ -25,10 +46,13 @@ const QRScanner = () => {
             console.log('Scanned Table ID:', tableId);
             console.log('User ID:', user._id);
 
+
             // Make API call with decoded parameters
             await addToTable({ restaurantId, tableId, userId: user._id, token: user.token });
 
-            Alert.alert('Success', 'You have been successfully added to the table');
+
+            handleScan(restaurantId, tableId);
+            navigation.navigate('Welcome');
 
             // Handle navigation or any other action after successful scan
         } catch (error) {
