61ce10af55bcc9966bdc1662f32a7c2d45a16f28
Fatima Siddiqui <fatimasidd440@gmail.com>
2024-06-11 22:45:12 +0500

    welcome screen issue fixed

--- mobileapp/Welcome.jsx
+++ mobileapp/Welcome.jsx
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import Header from './Header';
 import Footer from './Footer';
 import QRScanContext from './QRScanContext';
+import { navigation } from '@react-navigation/native';
 
 const Welcome = ({ navigation }) => {
     const { scannedRestaurant } = useContext(QRScanContext);
@@ -24,7 +25,7 @@ const Welcome = ({ navigation }) => {
                     <Text style={styles.viewMenuButtonText}>View Menu</Text>
                 </TouchableOpacity>
             </View>
-            <Footer />
+            <Footer navigation={navigation} activeIcon="solution1" />
         </View>
     );
 };
